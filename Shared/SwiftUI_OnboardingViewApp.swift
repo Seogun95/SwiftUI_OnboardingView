afe4c8d8a628a0676ecd70cf1334d4787b055637
SeonJungKim <ksj083895@gmail.com>
2021-05-18 09:32:01 +0500

    Initial Commit

---
+++ Shared/SwiftUI_OnboardingViewApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_OnboardingViewApp.swift
+//  Shared
+//
+//  Created by Seogun Kim on 2021/05/18.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_OnboardingViewApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
